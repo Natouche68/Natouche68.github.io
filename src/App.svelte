7156ef7dbfddb4aecf1c7d1b76ad0886455cdff1
Natouche68 <ngrossheny@gmail.com>
2023-07-12 11:14:02 +0200

    Add an hero

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,5 @@
-Hello World !
+<script lang="ts">
+	import Hero from './lib/Hero.svelte';
+</script>
+
+<Hero />
---
+++ src/lib/Hero.svelte
@@ -0,0 +1,70 @@
+<div class="hero">
+	<div class="title">
+		Hi,<br />
+		I'm Natouche68
+
+		<div class="shard" />
+	</div>
+	<div class="subtitle">Self-taught web developer</div>
+	<div class="continue">// scroll to contiune</div>
+</div>
+
+<style>
+	.hero {
+		width: 100vw;
+		height: 100vh;
+		background: #131313;
+		color: #ffffff;
+		position: relative;
+	}
+
+	.title {
+		position: absolute;
+		top: 148px;
+		left: 92px;
+		right: 92px;
+		/* bottom: 331px; */
+		font-size: 112px;
+		font-weight: 700;
+		line-height: 115.2px;
+		letter-spacing: -3.36px;
+		text-transform: uppercase;
+	}
+
+	.shard {
+		position: absolute;
+		top: -45px;
+		left: -36px;
+		height: 334px;
+		width: 3px;
+		background: linear-gradient(#00a3ff00 0%, #00a3ff 50%, #00a3ff00 100%);
+	}
+
+	.subtitle {
+		position: absolute;
+		top: 395px;
+		left: 92px;
+		right: 92px;
+		/* bottom: 289px; */
+		text-align: right;
+		font-size: 32px;
+		font-weight: 700;
+		line-height: 32px;
+		text-transform: capitalize;
+	}
+
+	.continue {
+		position: absolute;
+		top: 657px;
+		left: 0;
+		right: 0;
+		/* bottom: 29px; */
+		text-align: center;
+		font-size: 32px;
+		font-style: italic;
+		font-weight: 400;
+		line-height: 100%;
+		letter-spacing: -1.92px;
+		color: #a7a7a7;
+	}
+</style>
