02f004351b9a553567d9cdcf40c6d31bab4d0910
Natouche68 <ngrossheny@gmail.com>
2023-07-12 11:34:24 +0200

    Create the layout for the projects section

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import Hero from './lib/Hero.svelte';
 	import About from './lib/About.svelte';
+	import Projects from './lib/Projects.svelte';
 </script>
 
 <Hero />
 <About />
+<Projects />
---
+++ src/lib/Projects.svelte
@@ -0,0 +1,47 @@
+<div class="projects">
+	<div class="title">
+		Selected projects
+
+		<div class="shard" />
+	</div>
+	<div class="list" />
+</div>
+
+<style>
+	.projects {
+		width: 100%;
+		height: calc(100vh + 367px);
+		background: #131313;
+		color: #ffffff;
+		position: relative;
+		overflow: hidden;
+	}
+
+	.title {
+		position: absolute;
+		top: 133px;
+		left: 92px;
+		right: 92px;
+		/* bottom: 859px; */
+		font-size: 96px;
+		font-weight: 700;
+		line-height: 100%;
+		letter-spacing: -2.88px;
+		text-transform: uppercase;
+	}
+
+	.shard {
+		position: absolute;
+		top: -30px;
+		left: -36px;
+		height: 156px;
+		width: 3px;
+		background: linear-gradient(#00a3ff00 0%, #00a3ff 50%, #00a3ff00 100%);
+	}
+
+	.list {
+		width: 100%;
+		height: 100vh;
+		overflow: hidden;
+	}
+</style>
